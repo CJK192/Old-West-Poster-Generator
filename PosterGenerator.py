0063ab32f0c9519ceb1dbd6f3abc0fbb26bb2b9d
Cole Keller <colejkeller7@gmail.com>
2025-09-09 22:28:00 -0500

    Adding basic window
    I know I'm updating a lot but I want to make sure GitHub is working

--- PosterGenerator.py
+++ PosterGenerator.py
@@ -1,3 +1,11 @@
 from tkinter import *
 
 #Make sure you download the latest TKinter. Look up a YT video if you're confused
+
+
+
+root = Tk()
+
+root.title("Poster Generator")
+
+root.mainloop()
--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 Hello! If you're reading this file you recived the repo correctly :D
-We can add some description stuff here later if we want to
-or just delete this one.
+
+
+
