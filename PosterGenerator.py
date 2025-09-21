62400a29aac5ad528f3d7ca8d634cbfa2dbec53d
Cole Keller <colejkeller7@gmail.com>
2025-09-21 11:04:17 -0500

    Update PosterGenerator.py

--- PosterGenerator.py
+++ PosterGenerator.py
@@ -1,12 +1,35 @@
-from tkinter import *
-
-#Make sure you download the latest TKinter. Look up a YT video if you're confused
-
-WIDTH = 500
-HEIGHT = 500
+from PIL import Image
+from tkinter import Tk
+from tkinter import filedialog as fd
+from tkinter import ttk as tspmo
 
 root = Tk()
-root.title("Poster Generator")
-root.geometry(f"{WIDTH}x{HEIGHT}")
+root.title("app")
+root.geometry("300x300")
+
+directory = ""
+
+def open_text_file():
+    global directory
+    directory = fd.askopenfilename()
+    print(directory)
+
+def rotate_file():
+    global directory
+    if(directory == ""):
+        return 0
+    else:
+        imge = Image.open(directory)
+        n_img = imge.rotate(90)
+    
+        n_img.show()
+        n_img.save(directory, "PNG")
+
+enter_button = tspmo.Button(root, text="Select Image", command=open_text_file)
+enter_button.pack()
+
+flip_button = tspmo.Button(root, text="Rotate 90 degrees", command=rotate_file)
+flip_button.pack()
+
 
 root.mainloop()
