a25fb5ad0cf770d67b96f910bbd1d1787c0caa24
Cole Keller <colejkeller7@gmail.com>
2025-10-05 21:37:59 -0500

    Text Method Overhaul and New entries
    -Total Overhaul of text drawing method
    -Added 3 new entries
    -User can now add location, name, and amount

--- PosterGenerator.py
+++ PosterGenerator.py
@@ -16,66 +16,60 @@ root.geometry("400x400")
 
 WIDTH = 720
 HEIGHT = 1280
-FONT_SIZE_1 = 200.0
-
 
 directory = ""
-font_1 = ImageFont.truetype("WildWest.otf", FONT_SIZE_1)
+font_1 = ImageFont.truetype("WildWest.otf", 200.0)
 grain = Image.open("FinalFilmGrain.png").resize((800,800))
 Template = Image.open("BlankPosterTemplate.png").convert("L").resize((WIDTH, HEIGHT))
 TemplateWAmounts = Image.open("PosterWAmounts.png").convert("L").resize((WIDTH, HEIGHT))
 
-def text_maker(img):  
-    global font_1
-    text = entry.get().strip()
-    #test
-    print(f"Adding text to image: '{text}'")
+
+#Draws text onto img at (ypos/100) the way down the image with customizable margins, font-size, font, and color
+#IMPORTANT: ypos is like a slider from 0-100, 100 being at the bottom, 0 being at the top. NOT A MEASURE OF PIXELS!!
+
+def draw_text(text, img, ypos, margin=80, font_size=200.0, font="WildWest.otf", font_color="black"):  
+
+    global WIDTH
+
+    draw = ImageDraw.Draw(img)
+    text_len = draw.textlength(text, font=ImageFont.truetype(font, font_size))
+
+    if(text_len > WIDTH - margin * 2):
+        new_font_size = font_size
+        while(text_len > WIDTH - margin * 2):
+            new_font_size -= 1
+            text_len = draw.textlength(text, font=ImageFont.truetype(font, new_font_size))
+        draw.text((int(WIDTH / 2 - (text_len / 2)), int(((HEIGHT / 100)) * ypos)), text, fill=font_color, font=ImageFont.truetype(font, new_font_size))
+    else:
+         draw.text((int(WIDTH / 2 - (text_len / 2)), int(((HEIGHT / 100)) * ypos)), text, fill=font_color, font=ImageFont.truetype(font, font_size))
     
-    if text:
-        # draws text
-        # :)
-        draw = ImageDraw.Draw(img)
-        
-        
+   
 
-        if(len(text) > 6):
-            print("Stopped About where ou think")
-            font_1 = ImageFont.truetype("WildWest.otf", FONT_SIZE_1 - 8 * (len(text) - 6))
-        else:
-            font_1 = ImageFont.truetype("WildWest.otf", FONT_SIZE_1)
-
-        
-        text_len = draw.textlength(text, font=font_1)
-        
-        # Draw white text on top
-        print("Stopped here lmao")
-        draw.text((int(WIDTH / 2 - (text_len / 2)), int(((HEIGHT / 4)) * 3)), text, fill="black", font=font_1)
-        
-       # Ay respectfully imma comment this out 
-        print("Text successfully added to image!")
 
 def open_text_file():
     global directory
     directory = fd.askopenfilename()
-    print(f"Selected image: {directory}")
 
 def color_correct(dir):
     global grain
-    print(f"Selected template: {selected_template}")
+
     if(dir == ""):
         print("Please select an image first!")
         return 0
     else:
         # Open and process the image
         badimg = Image.open(dir)
-        img = ImageOps.exif_transpose(badimg).convert("L").resize((int(WIDTH / 2), int(HEIGHT * (5/12))))
+        img = ImageOps.exif_transpose(badimg).convert("L").resize((int(WIDTH / 2), int(HEIGHT * (9/24))))
         img.paste(grain, (0,0), mask=grain)
         img = ImageOps.colorize(img, black="#24221C", white="#D3B05F")
         temp = ImageOps.colorize(Template, black="#24221C", white="#D3B05F")
 
         #Bro it centers it ik it looks wierd but chill it lets us change the size of the poster
-        temp.paste(img, (int((WIDTH / 2) - ((WIDTH / 2) / 2)), int((HEIGHT / 2) - ((HEIGHT  * (5/12)) / 2))))
-        text_maker(temp)
+        temp.paste(img, (int((WIDTH / 2) - ((WIDTH / 2) / 2)), int((HEIGHT / 2) - ((HEIGHT  * (9/24)) / 2))))
+        draw_text(name_entry.get().strip(), temp, 70)
+        draw_text(location_entry.get().strip(), temp, 83, font_size=100.0)
+        draw_text(f"${bounty_entry.get().strip()}", temp, 90)
+        
         temp.show()
         
         
@@ -88,16 +82,27 @@ frame = ttk.Frame(root)
 frame.pack(pady=20, padx=20, fill='both', expand=True)
 
 # Put ALL widgets inside the frame
+Label(frame, text="Choose a template:").pack(pady=5)
 template_options = ["template1", "template2", "template3", "template4", "template5"]
 dropdown = ttk.Combobox(frame, values=template_options)
 dropdown.current(0)  # Set default value
 dropdown.pack(pady=10)
 selected_template = dropdown.get()
 
-Label(frame, text="Enter Name:").pack(pady=5)
-entry = Entry(frame, width=20, font=("Arial", 12))
-entry.pack(pady=10)
-entry.insert(0, "")
+Label(frame, text="Enter Name:").pack(pady=3)
+name_entry = Entry(frame, width=20, font=("Arial", 12))
+name_entry.pack(pady=5)
+name_entry.insert(0, "")
+
+Label(frame, text="Enter Location:").pack(pady=3)
+location_entry = Entry(frame, width=20, font=("Arial", 12))
+location_entry.pack(pady=5)
+location_entry.insert(0, "")
+
+Label(frame, text="Enter Bounty in Dollars (You don't need the $ sign):").pack(pady=3)
+bounty_entry = Entry(frame, width=20, font=("Arial", 12))
+bounty_entry.pack(pady=5)
+bounty_entry.insert(0, "")
 
 enter_button = ttk.Button(frame, text="Select Image", command=open_text_file)
 enter_button.pack(pady=10)
